afbf81b23593dcaaa8fa9f1e8781fa0932d86638
Bangjie <qq315035253@gmail.com>
2019-03-17 17:13:11 +0100

    use force simulation to compact thumbnails in attention cloud

--- client/eyecloud/src/app/random-pos.directive.ts
+++ client/eyecloud/src/app/random-pos.directive.ts
@@ -1,17 +1,162 @@
-import { Directive, ElementRef, Input } from '@angular/core';
+import { Directive, ElementRef, Input, OnChanges } from '@angular/core';
 import * as d3 from 'd3';
+import {Thumbnail} from "./attention-cloud/classes/Thumbnail";
+
 @Directive({
   selector: '[appRandomPos]'
 })
-export class RandomPosDirective {
-  @Input() posX: string;
-  @Input() posY: string;
+export class RandomPosDirective implements OnChanges{
+  @Input() thumbnailData: Thumbnail[];
+  @Input() imageUrl; string;
+  @Input() imageWidth: number;
+  @Input() imageHeight: number;
+  @Input() representation: string;
 
   constructor(private el: ElementRef) {}
 
-  ngOnInit() {
-    d3.select(this.el.nativeElement).attr('x', this.posX);
-    d3.select(this.el.nativeElement).attr('y', this.posY);
+  ngOnChanges() {
+
+    const multiplier = 2;
+
+    console.log(this.thumbnailData);
+    let svg = d3.select("svg")
+
+    // reset svg
+    svg.selectAll("*").remove();
+
+    // add background color
+    // svg.append("rect").
+    // attr("height", "100%").
+    // attr("width", "100%").
+    // attr("fill", "lightblue");
+
+    // create note data from thumbnail data
+    let nodeData = [];
+    for (let i = 0; i < this.thumbnailData.length; i++) {
+      let thumbnail = this.thumbnailData[i];
+      nodeData.push({
+        'name': thumbnail.id, 'r': thumbnail.croppingSize * multiplier,
+        'x': thumbnail.positionX, 'y': thumbnail.positionY,
+        'shiftX': thumbnail.styleX, 'shiftY': thumbnail.styleY
+      });
+    }
+
+    // create pattern for each thumbnail
+    let defs = svg.append("defs")
+      .selectAll("pattern")
+      .data(nodeData)
+      .enter()
+      .append("pattern")
+      .attr("width", function(d) { return d.r / multiplier; })
+      .attr("height", function(d) { return d.r / multiplier; })
+      .attr("patternTransform", function(d) {
+        return "translate(" + -d.shiftX + "," + -d.shiftY + ")";
+      })
+      .attr("id", function(d) {
+        return "pattern_" + d.name;
+      });
+
+    // add background image for cropping
+    defs.append("image")
+      .attr("xlink:href", this.imageUrl)
+      .attr("width", this.imageWidth)
+      .attr("height", this.imageHeight);
+
+    // produce forces
+    let attractForce = d3.forceManyBody().strength(50);
+
+    let collisionForce;
+    if (this.representation == "circular") {
+      collisionForce = d3.forceCollide().radius(function (d: any) {
+        return d.r / 2 + 1;
+      }).iterations(100);
+    } else {
+      collisionForce = d3.forceCollide().radius(function (d: any) {
+        return d.r / 2 * Math.sqrt(2) + 1;
+      }).iterations(100);
+    }
+
+    // force simulation
+    let simulation = d3.forceSimulation(nodeData).alphaDecay(0.01)
+      .force("attractForce",attractForce)
+      .force("collisionForce",collisionForce)
+      .force("center", d3.forceCenter(400, 300));
+
+    // produce nodes from node data
+    let node;
+    if (this.representation == "circular") {
+      node = svg.selectAll("circle").data(nodeData)
+        .enter().append("circle")
+        .attr("r",function(d){ return d.r / 2;})
+        .attr("cx",function(d){ return d.x;})
+        .attr("cy",function(d){ return d.y;})
+        .attr("fill", function(d) {
+          return "url(#pattern_" + d.name + ")";
+        })
+        .attr("stroke", "gray")
+        .attr("stroke-width", "2")
+        .call(d3.drag()
+          .on("start",dragstarted)
+          .on("drag",dragged)
+          .on("end",dragended));
+    } else {
+      node = svg.selectAll("rect").data(nodeData)
+        .enter().append("rect")
+        .attr("height",function(d){ return d.r;})
+        .attr("width",function(d){ return d.r;})
+        .attr("x",function(d){ return d.x;})
+        .attr("y",function(d){ return d.y;})
+        .attr("fill", function(d) {
+          return "url(#pattern_" + d.name + ")";
+        })
+        .attr("stroke", "gray")
+        .attr("stroke-width", "2")
+        .call(d3.drag()
+          .on("start",dragstarted)
+          .on("drag",dragged)
+          .on("end",dragended));
+    }
+
+    function dragstarted(d)
+    {
+      simulation.restart();
+      simulation.alpha(0.7);
+      d.fx = d.x;
+      d.fy = d.y;
+    }
+
+    function dragged(d)
+    {
+      d.fx = d3.event.x;
+      d.fy = d3.event.y;
+    }
+
+    function dragended(d)
+    {
+      d.fx = null;
+      d.fy = null;
+      simulation.alphaTarget(0.1);
+    }
+
+    function tickedCircle(){
+      node.attr("cx", function(d){ return d.x;})
+        .attr("cy", function(d){ return d.y;})
+    }
+
+    function tickedSquare() {
+      node.attr("x", function(d){ return d.x;})
+        .attr("y", function(d){ return d.y;})
+    }
+
+    // start simulation
+    if (this.representation == "circular") {
+      simulation.on("tick", tickedCircle);
+    } else {
+      simulation.on("tick", tickedSquare);
+    }
   }
 
+
 }
+
+
